1037dc90e733e9437e16e25487d1affb586e12e7
00011818 <wiut11818@gmail.com>
2021-11-29 14:47:06 +0500

    new code

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-print('hello world!')
-print('hello world!')
-print('hello world!')
-print('hello world!')
-print('hello world!')
-print('hello world!')
-print('hello world!')
-print('hello world!')
+print("CW deadline (sample: 20.02.2020 24:12:55)")
+
+time=str(input())
+
+def input_time(str):
+    return int(time[11:13]), int(time[14:16]), int(time[0:2]), int(time[3:5]), int(time[6:10])
+
+cw_deadline = []
+
+for i in input_time(time):
+    cw_deadline.append(i)
 
 
+print("CW submission (sample: 20.02.2020 24:12:55)")
+stime=str(input())
+
+submission_date = []
+for i in input_time(stime):
+    submission_date.append(i)
